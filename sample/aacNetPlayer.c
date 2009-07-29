3347cadaff01446840274a08026ef837ca1f7e0c
tipok <tipok@aefa4344-eeeb-11de-8f41-3b8bcad7c4de>
2009-07-29 20:36:11 +0000

    * ringbuffer implementation * curl easy works * TODO: pthreads?

--- sample/aacNetPlayer.c
+++ sample/aacNetPlayer.c
@@ -28,22 +28,32 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <stdarg.h>
+#include <sys/param.h>
+
+#include <curl/curl.h>
+#include <curl/types.h>
+#include <curl/easy.h>
+
+#include <pthread.h>
+
+
+
 
 #include "pvmp4audiodecoder_api.h"
 #include "e_tmp4audioobjecttype.h"
 #include "getactualaacconfig.h"
-#include "wav.h"
+#include "audio.h"
+#include "ringbuffer.h"
+
 #include "config.h"
 
 
 #define KAAC_NUM_SAMPLES_PER_FRAME      1024
-#define KAAC_MAX_STREAMING_BUFFER_SIZE  (PVMP4AUDIODECODER_INBUFSIZE * 1)
+#define KAAC_MAX_STREAMING_BUFFER_SIZE  (PVMP4AUDIODECODER_INBUFSIZE * 32)
 
 #define KCAI_CODEC_NO_MEMORY -1
 #define KCAI_CODEC_INIT_FAILURE -2
-
-FILE *ifile;
-FILE *ofile;
 
 /*
  * Debugging
@@ -81,30 +91,6 @@ void print_bytes(uint8_t *bytes, int len) {
     }
 }
 
-
-
-/*
- * Handle arguments
- * */
-static void parse_args(int argc, char **argv)
-{
-    if (argc>2) {
-        ifile = fopen (argv[1],"r");
-        if (ifile==NULL) {
-            fprintf (stderr, "input fopen error: %s\n", argv[1]);
-            exit(-1);
-        }
-
-        ofile = fopen (argv[2],"w");
-        if (ofile==NULL) {
-            fprintf (stderr, "output fopen error: %s\n", argv[2]);
-            exit(-1);
-        }
-            } else {
-        fprintf(stderr,"Usage: %s <in-file> <out-file>\n\n", argv[0]);
-        exit(-1);
-    }
-}
 
 
 int RetrieveDecodedStreamType(tPVMP4AudioDecoderExternal *pExt)
@@ -161,30 +147,179 @@ int bufferUpdate(FILE *fhandle, tPVMP4AudioDecoderExternal *pExt){
 }
 
 
+
+typedef struct {
+	/* decoder handler */
+	tPVMP4AudioDecoderExternal *pExt;
+	
+	/* decoder internal buffers */
+	void *pMem;
+
+	/* curl internal struct */
+	CURL *curl;
+	/* socket */
+	int sockfd;
+	
+	/* ringbuffer struct */
+	ringbuffer_t *rbuf;
+	uint32_t 	 bufSize;
+		
+	/* external buffers */
+	uint8_t *iInputBuf;
+	int16_t *iOutputBuf;
+} netplayer_t;
+
+
+void freeall(netplayer_t *handler){
+	if(handler->pMem != NULL)
+		PVMP4AudioDecoderResetBuffer(handler->pMem);
+	if(handler->pExt != NULL)
+		free(handler->pExt);
+	if(handler->iInputBuf != NULL)
+		free(handler->iInputBuf);
+	if(handler->iOutputBuf != NULL)
+		free(handler->iOutputBuf);
+	if(handler->curl != NULL)
+	    curl_easy_cleanup(handler->curl);
+	
+	free(handler);
+	handler=NULL;
+}
+
+void fail(netplayer_t *handler, const char *format, ...)
+{
+	va_list         pvar;
+	
+	va_start (pvar, format);
+	fprintf(stderr, format, pvar);
+	va_end (pvar);
+	fprintf(stderr, "\n");
+	freeall(handler);
+	exit(-1);
+}
+
+void fill_ringBuffer(netplayer_t *h, size_t bytes)
+{
+	size_t avaliable = ringbuffer_write_space(h->rbuf);
+	size_t iolen;
+	size_t total = 0;
+#define TCP_MTU		1600
+	char buf[TCP_MTU];
+	
+	if (bytes > avaliable)
+		fail(h, "no avaliable space in buffer");
+	
+	fprintf(stderr, "filling buffer: %u/%u %d percent", (int)bytes, (int)h->bufSize, (int)(bytes*100)/h->bufSize);
+	while (avaliable >= TCP_MTU && bytes <= ringbuffer_read_space(h->rbuf)) {
+		iolen = read(h->sockfd, buf, TCP_MTU);
+        if(0 == iolen)
+	         break;
+	    
+	    if( ringbuffer_write(h->rbuf, buf, iolen) != iolen)
+	    	fail(h, "not all data written to ring buffer");
+	    total += iolen;
+		fprintf(stderr, ". (%d)\n", (int)iolen);
+	}
+	fprintf(stderr, "\nfilled: (%d)\n", (int)total);
+}
+
+static size_t
+WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *data)
+{
+	size_t readsize = size * nmemb;
+	ringbuffer_t *rbuf = (ringbuffer_t *)data;
+	size_t avaliable = ringbuffer_write_space(rbuf);
+	
+	if (avaliable >= readsize) {
+		if( ringbuffer_write(rbuf, ptr, readsize) != readsize)
+			fprintf(stderr, "not all data written to ring buffer");
+		
+		fprintf(stderr, "read (%d)\n", (int)readsize);
+	}
+	
+	return readsize;
+}
+
+
+
 /*
  * Decoder main l00p
  * */
 int main(int argc, char **argv)
 {
-	tPVMP4AudioDecoderExternal *pExt = malloc(sizeof(tPVMP4AudioDecoderExternal));
-	uint8_t *iInputBuf = calloc(KAAC_MAX_STREAMING_BUFFER_SIZE, sizeof(uint8_t));
+	netplayer_t *player = calloc(1, sizeof(netplayer_t));
+	player->pExt = malloc(sizeof(tPVMP4AudioDecoderExternal));
+	tPVMP4AudioDecoderExternal *pExt = player->pExt;
+	
+	player->iInputBuf = calloc(KAAC_MAX_STREAMING_BUFFER_SIZE, sizeof(uint8_t));
 #ifdef AAC_PLUS
-    int16_t *iOutputBuf = calloc(4096, sizeof(int16_t));
+    player->iOutputBuf = calloc(4096, sizeof(int16_t));
 #else
-    int16_t *iOutputBuf = calloc(2048, sizeof(int16_t));
+    player->iOutputBuf = calloc(2048, sizeof(int16_t));
 #endif
-	int32_t memreq =  PVMP4AudioDecoderGetMemRequirements();
-	void *pMem = malloc(memreq);
 
-    if (pExt == NULL || iInputBuf == NULL || iOutputBuf == NULL || pMem == NULL) {
-    	fprintf(stderr, "not enough memory\n");
-        exit(-1);
+	int32_t memreq =  PVMP4AudioDecoderGetMemRequirements();
+	player->pMem = malloc(memreq);
+	void *pMem = player->pMem;
+
+	player->curl = curl_easy_init();
+	CURL *curl = player->curl;
+
+#define BUFFER_SIZE		131072
+#define PREBUFFER_SIZE	32768
+	player->bufSize = BUFFER_SIZE;
+	player->rbuf = ringbuffer_create(player->bufSize);
+	ringbuffer_t *rbuf = player->rbuf;
+
+    CURLcode res;
+
+
+	
+    if (pExt == NULL || player->iInputBuf == NULL || player->iOutputBuf == NULL
+    				 || pMem == NULL || curl == NULL || rbuf == NULL ) {
+    	fail(player, "not enough memory\n");
+    }
+	
+	/* Handle arguments */
+	if (argc == 3) {
+		/* argv[1] - inurl
+		 * argv[2] - output alsa device 
+		 * */
+    } else {
+        fail(player, "Usage: %s <URL> <out-file>\n\n", argv[0]);
     }
 	
 	
+	/* open network connection */
+    curl_easy_setopt(curl, CURLOPT_URL, "http://217.20.164.164:8000/bbc_radio1.aacp");
+    /* Do not do the transfer - only connect to host */
+    //curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 1L);
+    
+    /* send all data to this function  */
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    
+    /* we pass our 'chunk' struct to the callback function */
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)rbuf);
+    
+    /* some servers don't like requests that are made without a user-agent
+     * field, so we provide one */
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+
+
+	res = curl_easy_perform(curl);
+    if(CURLE_OK != res)
+    	fail(player, "CURL: %s\n", strerror(res));
 	
+	/* Extract the socket from the curl handle - we'll need it
+     * for waiting */
+        
+    fprintf(stderr, "[MAIN] sockfd %d\n", player->sockfd);
+    
+    
+    ringbuffer_reset(rbuf);
+
 	
-	parse_args(argc, argv);
 	
 #ifdef AAC_PLUS
 	fprintf(stderr, "[MAIN] AAC+ ENABLED\n");
@@ -193,8 +328,8 @@ int main(int argc, char **argv)
     pExt->pOutputBuffer_plus = NULL;
 #endif
 	
-    pExt->pInputBuffer			   = iInputBuf;
-    pExt->pOutputBuffer			   = iOutputBuf;
+    pExt->pInputBuffer			   = player->iInputBuf;
+    pExt->pOutputBuffer			   = player->iOutputBuf;
 	pExt->inputBufferMaxLength 	   = KAAC_MAX_STREAMING_BUFFER_SIZE;
     pExt->desiredChannels          = 2;
     pExt->inputBufferCurrentLength = 0;
@@ -209,8 +344,7 @@ int main(int argc, char **argv)
     
 
     if (PVMP4AudioDecoderInitLibrary(pExt, pMem) != 0) {
-    	fprintf(stderr, "init library failed\n");
-        exit(-1);
+    	fail(player, "init library failed\n");
     }
     
 	int32_t Status;
@@ -218,24 +352,23 @@ int main(int argc, char **argv)
 	int8_t  aIsFirstBuffer = 1;
 
 	/* seek to first adts sync */
-	fread (iInputBuf, 1, 2, ifile);
+	exit(1);
+//	read (iInputBuf, 1, 2, ifile);
 	fprintf(stderr, "sync search:");
-	while(1) {	
-		if (!((iInputBuf[0] == 0xFF)&&((iInputBuf[1] & 0xF6) == 0xF0))) {
-			fprintf(stderr, ".");
-			iInputBuf[0] = iInputBuf[1];
-			if(!fread(iInputBuf+1, 1, 1, ifile)) {
-				fprintf(stderr, "eof reached\n");
-				exit(-1);
-			}
-		} else {
-			if(!fread(iInputBuf+2, 1, KAAC_MAX_STREAMING_BUFFER_SIZE-2, ifile)) {
-				fprintf(stderr, "eof reached\n");
-				exit(-1);
-			}
-			break;
-		}
-	}
+//	while(1) {	
+//		if (!((player->iInputBuf[0] == 0xFF)&&((player->iInputBuf[1] & 0xF6) == 0xF0))) {
+//			fprintf(stderr, ".");
+//			iInputBuf[0] = iInputBuf[1];
+//			if(!fread(player->iInputBuf+1, 1, 1, ifile)) {
+//				fail(player, "eof reached\n");
+//			}
+//		} else {
+//			if(!fread(player->iInputBuf+2, 1, KAAC_MAX_STREAMING_BUFFER_SIZE-2, ifile)) {
+//				fail(player, "eof reached\n");
+//			}
+//			break;
+//		}
+//	}
 	fprintf(stderr, "found!\n");
    	pExt->inputBufferUsedLength=0;
    	pExt->inputBufferCurrentLength = KAAC_MAX_STREAMING_BUFFER_SIZE;
@@ -255,9 +388,9 @@ int main(int argc, char **argv)
         	}
    		}
    		
-   		if(!bufferUpdate(ifile, pExt)){
-   			exit(-1);
-   		}
+//   		if(!bufferUpdate(ifile, pExt)){
+//   			exit(-1);
+//   		}
    		pExt->inputBufferUsedLength = 0;
 	}	
 
@@ -265,9 +398,9 @@ int main(int argc, char **argv)
 	
 	/* main loop */
 	while (1) {
-		if(!bufferUpdate(ifile, pExt)){
-   			exit(0);
-   		}
+//		if(!bufferUpdate(ifile, pExt)){
+//   			exit(0);
+//   		}
    				
         Status = PVMP4AudioDecodeFrame(pExt, pMem);
 
@@ -318,7 +451,7 @@ int main(int argc, char **argv)
             					pExt->desiredChannels,
             					pExt->samplingRate);
 				/* create wav header */
-                aufile = open_audio_file(	ofile, 
+                aufile = open_audio_file(	argv[2], 
                 							pExt->samplingRate,
                 							pExt->desiredChannels,
                 							FMT_16BIT,
@@ -327,7 +460,7 @@ int main(int argc, char **argv)
 				aIsFirstBuffer=0;
 			}
 			/* save decoded frames */
-			write_audio_file(aufile, iOutputBuf, aOutputLength*pExt->desiredChannels, 0);
+			write_audio_file(aufile, player->iOutputBuf, aOutputLength*pExt->desiredChannels, 0);
 
     	} else if (MP4AUDEC_INCOMPLETE_FRAME == Status) {
         	fprintf(stderr, "[STATUS] Status: MP4AUDEC_INCOMPLETE_FRAME\n");
@@ -339,5 +472,6 @@ int main(int argc, char **argv)
 	}
 	
 	close_audio_file(aufile);
+	freeall(player); 
 	return 0;
 }
